a7b7e1027216e0158517495df040099ad7dab074
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-06 10:41:18 +0200

    add imports and exports of images

---
+++ public/asstes/index.tsx
@@ -0,0 +1,23 @@
+import imag1 from "./images/img1.jpg";
+import imag2 from "./images/img2.jpg";
+import imag3 from "./images/imag3.jpg";
+import imag4 from "./images/imag4.jpg";
+import imag5 from "./images/imag5.jpg";
+import imag6 from "./images/imag6.jpg";
+import imag7 from "./images/imag7.jpg";
+import imag8 from "./images/imag8.jpg";
+import imag9 from "./images/imag9.jpg";
+import imag10 from "./images/imag10.jpg";
+
+export {
+  imag1,
+  imag2,
+  imag3,
+  imag4,
+  imag5,
+  imag6,
+  imag7,
+  imag8,
+  imag9,
+  imag10,
+};
