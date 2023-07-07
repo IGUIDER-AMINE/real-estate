7da88b154b009741d2426b856967f6be709461b8
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-07 09:39:59 +0200

    finish navbar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,37 +4,45 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full flex items-center justify-between bg-red-200 container mx-auto">
+    <div className="w-full text-[15px] py-4 flex items-center justify-between container mx-auto">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <span className="text-3xl font-titleFont">IGUIDER</span>
+        <span className="text-3xl font-titleFont">Propertymax</span>
       </motion.div>
-      <div className="inline-flex">
-        <ul className="flex gap-x-5">
+      <div>
+        <ul className="flex gap-x-5 text-[#7F7F80]">
           <li>
             <Link legacyBehavior href="#home">
-              home
+              how it works
             </Link>
           </li>
           <li>
             <Link legacyBehavior href="#home">
-              home
+              About Us
             </Link>
           </li>
           <li>
             <Link legacyBehavior href="#home">
-              home
+              Our Agent
             </Link>
           </li>
           <li>
             <Link legacyBehavior href="#home">
-              home
+              Contact Us
             </Link>
           </li>
         </ul>
+      </div>
+      <div className="flex gap-2">
+        <button className="border w-[170px] border-slate-600 p-2 rounded-full">
+          Free Consultation
+        </button>
+        <button className="border w-[170px] bg-[#0397F9] text-white p-2 rounded-full">
+          Configure now
+        </button>
       </div>
     </div>
   );
