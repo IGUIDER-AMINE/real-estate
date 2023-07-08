a5f451630d26d906219787acc0feebb19eabe5d9
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-08 19:51:04 +0200

    finish products section

---
+++ components/elements/ProductsInfos.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+}
+
+const ProductsInfos = ({ id, title, description, image }: Props) => {
+  return (
+    <div className="space-y-16">
+      <hr className="bg-[#F1F1F1] h-[2.5px]" />
+      <div className="flex items-center justify-between space-x-2">
+        <div className="space-y-6">
+          <div className="text-2xl font-titleFont font-bold text-slate-800">
+            {id}
+          </div>
+          <h1 className="text-3xl font-titleFont font-bold text-slate-800">
+            {title}
+          </h1>
+          <p className="text-slate-800 font-medium lg:max-w-[65%]">
+            {description}
+          </p>
+          <button className="border p-4 font-semibold text-slate-800  border-slate-800 rounded-full">
+            Learn More
+          </button>
+        </div>
+        <div className="hidden md:block min-h-[300px] lg:min-h-[500px] min-w-[50%] relative">
+          <Image
+            src={image}
+            alt="image estate"
+            className="object-cover rounded-2xl h-full"
+            quality={100}
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsInfos;
--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import ProductsInfos from "../elements/ProductsInfos";
 
 const Products = () => {
-  return <section id="Products">Products</section>;
+  return (
+    <section id="Products" className="space-y-16 container mx-auto px-4">
+      <ProductsInfos
+        id="01"
+        title="Captivating Home Views"
+        description="Immerse in Serne Scenic Beauty - Experience captivating home views
+            that soothe the soul and inspire tranquility."
+        image="/asstes/images/img3.jpg"
+      />
+      <ProductsInfos
+        id="02"
+        title="Contemporary Bliss"
+        description="Immerse in modern luxury with our exquisite range of upscale amenities, defining the pinnacle of contemporary lifestyle."
+        image="/asstes/images/img4.jpg"
+      />
+      <ProductsInfos
+        id="03"
+        title="Prime Locations"
+        description="Explore prime locations adorned with convenience and value, placing you at the heart of desirable neighbothoods."
+        image="/asstes/images/img6.jpg"
+      />
+    </section>
+  );
 };
 
 export default Products;
